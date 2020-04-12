57038afe60c5fe42463f6e3e1e6e1ee4366683c9
mmurray22 <michaelamurray2357@gmail.com>
2020-04-12 10:25:15 -0700

    Final changes before very first commit

--- capsules/src/qdec.rs
+++ capsules/src/qdec.rs
@@ -1,4 +1,24 @@
-//! Capsule for QDEC 
+//! Provides userspace access to the Qdec on a board.
+//!
+//! Usage
+//! -----
+//!
+//! ````
+//! let qdec = static_init!(
+//!     capsules::qdec::Qdec<'static>,
+//!     capsules::qdec::QdecInterface::new(&nrf52::qdec::QDEC,
+//!                                         kernel::Grant::create())
+//! );
+//! kernel::hil::QdecDriver.set_client(qdec);
+//! ````
+//!
+//! #Number of Qdecs
+//! The number of qdecs available depends on the
+//! board/microcontroller
+//!
+//! #Interrupt Spurred Readings versus Regular Readings
+//! An application can either enable interrupts to get the
+//! accumulation value or manually read it whenever it wants
 
 use crate::driver;
 use kernel::hil;
@@ -8,31 +28,34 @@ use kernel::{AppId, Callback, ReturnCode, Driver, Grant};
 
 pub const DRIVER_NUM: usize = driver::NUM::Qdec as usize;
 
+/// This struct contains the resources necessary for the QdecInterface
 pub struct QdecInterface<'a> {
     driver: &'a dyn hil::qdec::QdecDriver,
     apps: Grant<App>,
-    curr_acc: u32,
 }
 
 #[derive(Default)]
+/// This struct contains the necessary fields for an app
 pub struct App {
     callback: Option<Callback>,
     subscribed: bool,
 }
 
 impl QdecInterface<'a> {
+    /// Create a new instance of the QdecInterface
     pub fn new (
-        driver: &'a dyn  hil::qdec::QdecDriver, 
+        driver: &'a dyn  hil::qdec::QdecDriver,
         grant: Grant<App>,
     ) -> QdecInterface<'a> {
         QdecInterface {
             driver: driver,
             apps: grant,
-            curr_acc: 0,
         }
     }
 
-    fn configure_callback(&self, callback: Option<Callback>, app_id: AppId) -> ReturnCode {
+    /// Configure callback
+    fn configure_callback(&self, callback: Option<Callback>, app_id: AppId)
+        -> ReturnCode {
         self.driver.enable_interrupts();
         self.apps
             .enter(app_id, |app, _| {
@@ -44,6 +67,8 @@ impl QdecInterface<'a> {
 }
 
 impl hil::qdec::QdecClient for QdecInterface<'a> {
+    /// Goes through all the apps and if the app is
+    /// subscribed then it sends back the acc value
     fn sample_ready (&self) {
         for cntr in self.apps.iter() {
             cntr.enter(|app, _| {
@@ -56,6 +81,7 @@ impl hil::qdec::QdecClient for QdecInterface<'a> {
 }
 
 impl Driver for QdecInterface<'a> {
+    /// Subscribes a client to (newly enabled) interrupts
     fn subscribe(
         &self,
         subscribe_num: usize,
@@ -63,29 +89,24 @@ impl Driver for QdecInterface<'a> {
         app_id: AppId,
     ) -> ReturnCode {
         match subscribe_num {
-            // subscribe to qdec reading with callback
             0 => self.configure_callback(callback, app_id),
             _ => ReturnCode::ENOSUPPORT,
         }
     }
 
+    /// Command switch statement for various essential processes
     fn command (&self, command_num: usize, _: usize, _: usize, appid: AppId) -> ReturnCode {
         match command_num {
-            //dummy value
+            /// dummy value
             0 => ReturnCode::SUCCESS,
-            //enable qdec
+            /// enable qdec
             1 => self.driver.enable(),
-            //get qdec acc
+            /// get qdec acc
             2 =>
               ReturnCode::SuccessWithValue {
                 value: self.get_acc() as usize,
               },
-            //
-            /*TODO: any others? 3 =>
-              ReturnCode::SuccessWithValue {
-                value: 
-              }*/
-            //default
+            /// default
             _ => ReturnCode::ENOSUPPORT
         }
     }
--- kernel/src/hil/qdec.rs
+++ kernel/src/hil/qdec.rs
@@ -1,22 +1,26 @@
-/// A basic interface for a QDEC compatible chip
-///
-/// This trait provides a stanfard interface for chips that 
-/// contain a quadrature encoder. Any interface functions that
-/// a chip cannot implement can be ignored by the chip capsule 
-/// and an error will automatically be returned.
+//! Interface for a Qdec compatible chip
+//!
+//! This trait provides a stanfard interface for chips a 
+//! quadrature decoder. Any interface functions that
+//! a chip cannot implement can be ignored by the chip capsule 
 
 use crate::returncode::ReturnCode;
 
 pub trait QdecDriver {
+  /// Sets the client which will receive interrupts
   fn set_client(&self, client: &'static dyn QdecClient);  
  
+  /// Enables the SAMPLERDY interrupt
   fn enable_interrupts (&self);
   
+  /// Enables the Qdec, returning error if Qdec does not exist
   fn enable (&self) -> ReturnCode;
 
+  /// Gets the accumulation value
   fn get_acc (&self) -> u32;
 }
 
 pub trait QdecClient {
+    /// Callback function
     fn sample_ready (&self);
 }
