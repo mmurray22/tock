e75a2418d6402acfe61d5811ba76f57fe0862ea4
tock <michaelamurray2357@gmail.com>
2021-01-13 15:13:18 +0800

    Initial rough interface compiles. Does not yet qualify as a draft

--- capsules/src/system_call_interface.rs
+++ capsules/src/system_call_interface.rs
@@ -2,8 +2,6 @@
 //! device
 
 use core::cell::Cell;
-use crate::driver;
-use core::cmp;
 use kernel::common::cells::{OptionalCell, TakeCell};
 use kernel::hil::spi;
 use kernel::{AppId, AppSlice, Callback, Driver, Grant, ReturnCode, Shared};
@@ -13,16 +11,16 @@ use kernel::{AppId, AppSlice, Callback, Driver, Grant, ReturnCode, Shared};
 // needs to be sent to a remote device. If it can be handled locally, then the 
 // normal system call proceeds and if not, then 
 
-pub struct DetermineRoute<'a> {
+pub struct DetermineRoute {
   driver_num: usize,
   system_call_num: usize,
 }
 
-impl<'a> DetermineRoute<'a> {
+impl DetermineRoute {
   pub fn new(
       driver_num: usize,
       system_call_num: usize
-  ) -> DetermineRoute<'a> {
+  ) -> DetermineRoute {
     DetermineRoute {
       driver_num: driver_num,
       system_call_num: system_call_num,
@@ -30,8 +28,17 @@ impl<'a> DetermineRoute<'a> {
   }
 
   pub fn determine_route(&self) -> usize {
-    let usize route = 1;
+    let route : usize = 1;
     route
+  }
+
+  pub fn create_read_buffer(
+      &self, 
+      arg_one: usize, 
+      arg_two: usize, 
+      arg_three: usize, 
+      arg_four: usize) -> [usize; 6] {
+    [self.driver_num, self.system_call_num, arg_one, arg_two, arg_three, arg_four] 
   }
 }
 
@@ -46,8 +53,8 @@ enum Status {
 pub struct RemoteSystemCall<'a> {
   spi: &'a dyn spi::SpiMasterDevice,
   pass_buffer: TakeCell<'static, [u8]>,
-  write_buffer: TakCell<'static, [u8]>,
-  read_buffer: TakeCell<'static, [u8]>,
+  write_buffer: TakeCell<'static, [u8]>,
+  read_buffer: TakeCell<'static, [usize; 6]>,
   status: Cell<Status>,
   driver_num: usize,
   system_call_num: usize,
@@ -57,7 +64,7 @@ impl<'a> RemoteSystemCall<'a> {
   pub fn new(
       spi: &'a dyn spi::SpiMasterDevice,
       pass_buffer: &'static mut [u8],
-      read_buffer: &'static mut [u8],
+      read_buffer: &'static mut [usize; 6],
       driver_num: usize,
       system_call_num: usize
   ) -> RemoteSystemCall<'a> {
@@ -77,25 +84,26 @@ impl<'a> RemoteSystemCall<'a> {
     }
   }
   
-  
-  pub fn send_data(&self) -> usize {
+  pub fn send_data(&self) -> ReturnCode {
       if self.status.get() == Status::Idle {
-          let error = self.pass_buffer.map_or_else(
+          self.pass_buffer.take().map_or_else(
               || panic!("There is no spi pass buffer!"),
               |pass_buffer| {
                   self.spi.read_write_bytes(pass_buffer, None, 1);
+                  self.status.set(Status::Sending);
               },
           );
+          ReturnCode::SUCCESS
       } else {
           ReturnCode::EBUSY
       }
   }
 
-  pub fn receive_data() -> ReturnCode {
-  }
-
-  fn create_arg_buf (&self, arg_one: usize, arg_two: usize, arg_three: usize, arg_four: usize) {
-  
+  pub fn receive_data(&self) -> Option<&'static mut [usize; 6]> {
+      if self.status.get() == Status::Sending {
+        self.read_buffer.take()
+      } else {
+          None
+      }
   }
 }
-
